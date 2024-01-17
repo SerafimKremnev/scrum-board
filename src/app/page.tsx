ba44e488bea32b3df5a5d943334d1ca57bc16911
Alexey <alexey.swn@gmail.com>
2024-01-17 14:41:46 +1000

    update tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Button from '@/shared/ui/button/ui';
-
 export default function Home() {
   return (
     <>
       <main>
         <h1>Next.js Project</h1>
-        <Button>Кнопка</Button>
       </main>
     </>
   );
